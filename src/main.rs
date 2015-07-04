ed748c68ec884ce19520d12a08ea8402aa7c8cf3
Mark Rowe <mrowe@bdash.net.nz>
2015-07-04 03:45:18 -0700

    Optimize!
    Introduce LinkedInstruction. This is the same as Instruction except
    LoopStart / LoopEnd have required end / start parameters for less
    indirection.
    
    Introduce an optimization pass that merges adjacent move / modify
    instructions into a single operation with an amount > 1.
    
    Move to unchecked vector accessors where possible when accessing
    instructions and the tape. This requires marking many things as unsafe.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-#![feature(vecmap)]
+#![feature(vecmap, core_intrinsics)]
+
+extern crate itertools;
 
 use std::collections::VecMap;
 use std::io::{Result, Read, Write, stdout};
 use std::fs::File;
 use std::path::Path;
+use itertools::Itertools;
+
 
 #[inline(never)]
 fn load_file(input_file_path: &str) -> Result<Vec<u8>> {
@@ -13,19 +17,19 @@ fn load_file(input_file_path: &str) -> Result<Vec<u8>> {
   Ok(contents)
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Direction {
   Left,
   Right
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Operation {
   Add,
   Subtract
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Instruction {
   Move(Direction, usize),
   Modify(Operation, u8),
@@ -33,6 +37,16 @@ enum Instruction {
   Input,
   LoopStart { end: Option<usize> },
   LoopEnd { start: Option<usize> },
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum LinkedInstruction {
+  Move(Direction, usize),
+  Modify(Operation, u8),
+  Output,
+  Input,
+  LoopStart { end: usize },
+  LoopEnd { start: usize },
 }
 
 impl Instruction {
@@ -51,19 +65,44 @@ impl Instruction {
   }
 }
 
+impl LinkedInstruction {
+  fn from_instruction(instruction: &Instruction) -> LinkedInstruction {
+    match *instruction {
+      Instruction::Move(direction, amount) => LinkedInstruction::Move(direction, amount),
+      Instruction::Modify(operation, amount) => LinkedInstruction::Modify(operation, amount),
+      Instruction::Output => LinkedInstruction::Output,
+      Instruction::Input => LinkedInstruction::Input,
+      Instruction::LoopStart { end: Some(end) } => LinkedInstruction::LoopStart { end: end },
+      Instruction::LoopEnd { start: Some(start) } => LinkedInstruction::LoopEnd { start: start},
+      _ => panic!(),
+    }
+  }
+}
+
 #[inline(never)]
 fn compile(source: Vec<u8>) -> Vec<Instruction> {
-  source.into_iter().filter_map(|token| {
-    if token as char == '\n' {
-      None
-    } else {
-      Instruction::from_token(token)
+  source.into_iter().filter_map(Instruction::from_token).collect()
+}
+
+#[inline(never)]
+fn optimize(instructions: Vec<Instruction>) -> Vec<Instruction> {
+  instructions.iter().group_by(|&instruction| instruction).flat_map(|(&key, group)| {
+    match key {
+      Instruction::Move(direction, _) => {
+        vec![Instruction::Move(direction, group.len())]
+      },
+      Instruction::Modify(operation, _) => {
+        group.chunks(u8::max_value() as usize).map(|g| {
+          Instruction::Modify(operation, g.len() as u8)
+        }).collect()
+      },
+      _ => group.iter().map(|&instruction| *instruction).collect()
     }
   }).collect()
 }
 
 #[inline(never)]
-fn link(source: Vec<Instruction>) -> Vec<Instruction> {
+fn link(source: Vec<Instruction>) -> Vec<LinkedInstruction> {
   let mut loop_starts = Vec::new();
   let mut loop_ends = VecMap::new();
   let phase1: Vec<(usize, Instruction)>;
@@ -88,61 +127,69 @@ fn link(source: Vec<Instruction>) -> Vec<Instruction> {
     match instruction {
       Instruction::LoopStart { end: None } => {
         let end = loop_ends.get(&i).unwrap();
-        Instruction::LoopStart { end: Some(*end) }
+        LinkedInstruction::LoopStart { end: *end }
       },
-      _ => instruction,
+      _ => LinkedInstruction::from_instruction(&instruction)
       }
   }).collect()
 }
 
 #[inline(never)]
-fn execute(instructions: Vec<Instruction>) {
-  let mut tape = vec![0u8 ;30000];
+unsafe fn execute(instructions: Vec<LinkedInstruction>) {
+//  println!("{:?}", instructions);
+  let mut output = Vec::with_capacity(256);
+  let mut tape = vec![0u8; 3000];
   let mut tape_head = 0;
   let mut ip = 0;
   while ip < instructions.len() {
-    let instruction = &instructions[ip];
+    let instruction = instructions.get_unchecked(ip);
 
+//    println!("{:?}", instruction);
     ip = match *instruction {
-      Instruction::Modify(ref operation, amount) => {
+      LinkedInstruction::Modify(ref operation, amount) => {
         tape[tape_head] = match *operation {
-          Operation::Add => tape[tape_head].wrapping_add(amount),
-          Operation::Subtract => tape[tape_head].wrapping_sub(amount),
+          Operation::Add => tape.get_unchecked(tape_head).wrapping_add(amount),
+          Operation::Subtract => tape.get_unchecked(tape_head).wrapping_sub(amount),
         };
         ip + 1
       },
-      Instruction::Move(ref direction, amount) => {
+      LinkedInstruction::Move(ref direction, amount) => {
         match *direction {
           Direction::Left => tape_head -= amount,
           Direction::Right => tape_head += amount,
         }
         ip + 1
       },
-      Instruction::LoopStart { end: Some(end)} => {
-        if tape[tape_head] == 0 {
+      LinkedInstruction::LoopStart { end } => {
+        if *tape.get_unchecked(tape_head) == 0 {
           end + 1
         } else {
           ip + 1
         }
       },
-      Instruction::LoopEnd { start: Some(start)} => {
-        if tape[tape_head] != 0 {
+      LinkedInstruction::LoopEnd { start } => {
+        if *tape.get_unchecked(tape_head) != 0 {
           start + 1
         } else {
           ip + 1
         }
       },
-      Instruction::Output => {
-        stdout().write(&tape[tape_head..tape_head + 1]).unwrap();
+      LinkedInstruction::Output => {
+        let c = *tape.get_unchecked(tape_head);
+        output.push(c);
+        if c as char == '\n' {
+          stdout().write(&output[..]).unwrap();
+          output.clear();
+        }
         ip + 1
       }
-      _ => panic!()
+      _ => std::intrinsics::unreachable(),
     }
   }
 }
 
 fn main() {
   let source = load_file("input.bf").unwrap();
-  let instructions = link(compile(source));
-  execute(instructions);
+  let instructions = link(optimize(compile(source)));
+  unsafe { execute(instructions) };
 }
