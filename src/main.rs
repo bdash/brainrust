12cb0c76119cc903e96aad865ac1ce324f8877d9
Mark Rowe <mrowe@bdash.net.nz>
2015-07-04 12:22:02 -0700

    Special Modify / Move into Add, Subtract, MoveLeft and MoveRight to remove an extra conditional step in their evaluation.

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,11 @@ fn load_file(input_file_path: &str) -> Result<Vec<u8>> {
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
-enum Direction {
-  Left,
-  Right
-}
-
-#[derive(Debug, PartialEq, Copy, Clone)]
-enum Operation {
-  Add,
-  Subtract
-}
-
-#[derive(Debug, PartialEq, Copy, Clone)]
 enum Instruction {
-  Move(Direction, usize),
-  Modify(Operation, u8),
+  MoveLeft(usize),
+  MoveRight(usize),
+  Add(u8),
+  Subtract(u8),
   Output,
   Input,
   LoopStart { end: Option<usize> },
@@ -41,8 +31,10 @@ enum Instruction {
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum LinkedInstruction {
-  Move(Direction, usize),
-  Modify(Operation, u8),
+  MoveLeft(usize),
+  MoveRight(usize),
+  Add(u8),
+  Subtract(u8),
   Output,
   Input,
   LoopStart { end: usize },
@@ -52,10 +44,10 @@ enum LinkedInstruction {
 impl Instruction {
   fn from_token(token: u8) -> Option<Instruction> {
     match token as char {
-      '<' => Some(Instruction::Move(Direction::Left, 1)),
-      '>' => Some(Instruction::Move(Direction::Right, 1)),
-      '+' => Some(Instruction::Modify(Operation::Add, 1)),
-      '-' => Some(Instruction::Modify(Operation::Subtract, 1)),
+      '<' => Some(Instruction::MoveLeft(1)),
+      '>' => Some(Instruction::MoveRight(1)),
+      '+' => Some(Instruction::Add(1)),
+      '-' => Some(Instruction::Subtract(1)),
       '.' => Some(Instruction::Output),
       ',' => Some(Instruction::Input),
       '[' => Some(Instruction::LoopStart { end: None }),
@@ -68,8 +60,10 @@ impl Instruction {
 impl LinkedInstruction {
   fn from_instruction(instruction: &Instruction) -> LinkedInstruction {
     match *instruction {
-      Instruction::Move(direction, amount) => LinkedInstruction::Move(direction, amount),
-      Instruction::Modify(operation, amount) => LinkedInstruction::Modify(operation, amount),
+      Instruction::MoveLeft(amount) => LinkedInstruction::MoveLeft(amount),
+      Instruction::MoveRight(amount) => LinkedInstruction::MoveRight(amount),
+      Instruction::Add(amount) => LinkedInstruction::Add(amount),
+      Instruction::Subtract(amount) => LinkedInstruction::Subtract(amount),
       Instruction::Output => LinkedInstruction::Output,
       Instruction::Input => LinkedInstruction::Input,
       Instruction::LoopStart { end: Some(end) } => LinkedInstruction::LoopStart { end: end },
@@ -88,12 +82,16 @@ fn compile(source: Vec<u8>) -> Vec<Instruction> {
 fn optimize(instructions: Vec<Instruction>) -> Vec<Instruction> {
   instructions.iter().group_by(|&instruction| instruction).flat_map(|(&key, group)| {
     match key {
-      Instruction::Move(direction, _) => {
-        vec![Instruction::Move(direction, group.len())]
-      },
-      Instruction::Modify(operation, _) => {
+      Instruction::MoveLeft(_) => vec![Instruction::MoveLeft(group.len())],
+      Instruction::MoveRight(_) => vec![Instruction::MoveRight(group.len())],
+      Instruction::Add(_) => {
         group.chunks(u8::max_value() as usize).map(|g| {
-          Instruction::Modify(operation, g.len() as u8)
+          Instruction::Add(g.len() as u8)
+        }).collect()
+      },
+      Instruction::Subtract(_) => {
+        group.chunks(u8::max_value() as usize).map(|g| {
+          Instruction::Subtract(g.len() as u8)
         }).collect()
       },
       _ => group.iter().map(|&instruction| *instruction).collect()
@@ -145,33 +143,22 @@ unsafe fn execute(instructions: Vec<LinkedInstruction>) {
     let instruction = instructions.get_unchecked(ip);
 
 //    println!("{:?}", instruction);
-    ip = match *instruction {
-      LinkedInstruction::Modify(ref operation, amount) => {
-        tape[tape_head] = match *operation {
-          Operation::Add => tape.get_unchecked(tape_head).wrapping_add(amount),
-          Operation::Subtract => tape.get_unchecked(tape_head).wrapping_sub(amount),
-        };
-        ip + 1
-      },
-      LinkedInstruction::Move(ref direction, amount) => {
-        match *direction {
-          Direction::Left => tape_head -= amount,
-          Direction::Right => tape_head += amount,
-        }
-        ip + 1
-      },
+    match *instruction {
+      LinkedInstruction::Add(amount) => tape[tape_head] = tape.get_unchecked(tape_head).wrapping_add(amount),
+      LinkedInstruction::Subtract(amount) => tape[tape_head] = tape.get_unchecked(tape_head).wrapping_sub(amount),
+      LinkedInstruction::MoveLeft(amount) => tape_head -= amount,
+      LinkedInstruction::MoveRight(amount) => tape_head += amount,
+
       LinkedInstruction::LoopStart { end } => {
         if *tape.get_unchecked(tape_head) == 0 {
-          end + 1
-        } else {
-          ip + 1
+          ip = end + 1;
+          continue
         }
       },
       LinkedInstruction::LoopEnd { start } => {
         if *tape.get_unchecked(tape_head) != 0 {
-          start + 1
-        } else {
-          ip + 1
+          ip = start + 1;
+          continue
         }
       },
       LinkedInstruction::Output => {
@@ -181,10 +168,10 @@ unsafe fn execute(instructions: Vec<LinkedInstruction>) {
           stdout().write(&output[..]).unwrap();
           output.clear();
         }
-        ip + 1
       }
       _ => std::intrinsics::unreachable(),
     }
+    ip += 1
   }
 }
 
